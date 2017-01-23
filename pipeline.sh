40fcdaf025a45aac4956ac97f19284177d0fefc9
System Manager <manager@bl8vbox.unassigned-domain>
2017-01-23 12:50:59 +0000

    pipeline takes arguments to select a set of genes/proteins

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,18 @@
-#! /bin/bash
+#! /usr/bin/env python
 
-grep ">" -h *.fas | sort | uniq -c
+import sys
+import os
+
+# Please use the proper extentions for the DNA sequences (000.fnt) and the Amino Acid sequences (000.faa)
+string=sys.argv[1]
+print string[:-7]
+
+
+string="%s*.fnt" % (string[:-7])
+print string
+#rm analysis.fnt
+#rm analysis.faa
+#cat string***.fnt > analysis.fnt
+#cat *.faa > analysis.faa
+os.system('grep ">" -h '+string+" | sort | uniq -c") 
 
