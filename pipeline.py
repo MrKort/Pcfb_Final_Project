34f53e1cdbeacb5970930184c9a68b2e469d29bf
Helen <helen@NotebookHelen>
2017-01-24 14:42:01 +0100

    Original py

--- pipeline.py
+++ pipeline.py
@@ -18,17 +18,5 @@ os.system('cat '+string+" > workdir/input.fas")
 print "Starting Multiple Sequence Alingment"
 
 # Start the Multiple Sequence Alignments
-os.system('mafft --localpair --maxiterate 1000 --op 5 --lop 1 workdir/input.fas > workdir/mafft_output2.fas')
-os.system('mafft --localpair --maxiterate 1000 --op 15 --lop 5 workdir/input.fas > workdir/mafft_output3.fas')
-os.system('mafft --localpair --maxiterate 1000 --op 25 --lop 15 workdir/input.fas > workdir/mafft_output4.fas')
-os.system('mafft --localpair --maxiterate 1000 --op 25 --lop 25 workdir/input.fas > workdir/mafft_output5.fas')
-os.system('mafft --localpair --maxiterate 1000 --op 250 --lop 150 workdir/input.fas > workdir/mafft_output6.fas')
-os.system('muscle -gapopen -15 -gapextend -5 -in workdir/input.fas -out workdir/muscle_output2.fas')
-os.system('mafft --localpair --maxiterate 1000 workdir/input.fas > workdir/mafft_output.fas')
+os.system('mafft workdir/input.fas > workdir/mafft_output.fas')
 os.system('muscle -in workdir/input.fas -out workdir/muscle_output.fas')
-
-os.system('python stable.py workdir/input.fas workdir/muscle_output.fas > workdir/muscle_output_stable.fas')
-os.system('python stable.py workdir/input.fas workdir/muscle_output2.fas > workdir/muscle_output_stable2.fas')
-
-os.system('cat workdir/muscle_output_stable.fas workdir/muscle_output_stable2.fas > workdir/muscle.fas')
-os.system('cat workdir/mafft_output.fas workdir/mafft_output2.fas workdir/mafft_output3.fas workdir/mafft_output4.fas workdir/mafft_output5.fas workdir/mafft_output6.fas > workdir/mafft.fas')
