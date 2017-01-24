4d0c95486f09c3e32a6f9cc8283fb93e4f0930fd
System Manager <manager@bl8vbox.unassigned-domain>
2017-01-24 13:32:41 +0000

    Sequence counter and MAFFT reformatter

--- pipeline.py
+++ pipeline.py
@@ -8,16 +8,40 @@ import sys
 #Be sure to enclose it in apostorphes like: "file*.faa", if you wish to select all files of a certain type
 string=sys.argv[1]
 
-print "Showing all individual sequences from ", string, " files:"
+# Count number of sequences and write sequence names to seq_names.txt
+os.system('grep ">" -ho '+string+" | wc -l > workdir/seq_names.txt")
+os.system('grep ">" -h '+string+" | sort | uniq -c >> workdir/seq_names.txt")
+
+# Obtain and store the number of sequences from seq_names.txt
+Seq_count = open("workdir/seq_names.txt")
+lines_file = Seq_count.readlines()
+seq_num = int(lines_file[0])
+Seq_count.close()
+
+# Print sequences to screen
+print
+print "Showing all", seq_num, "individual sequences from", string, "files:"
 os.system('grep ">" -h '+string+" | sort | uniq -c") 
-os.system('grep ">" -h '+string+" | sort | uniq -c > workdir/seq_names.txt")
 
 # Concatenate all files into a single inputfile for the pipeline
-print "Concatenating all ", string, "files"
+print
+print "Concatenating all", string, "files."
 os.system('cat '+string+" > workdir/input.fas")
-print "Starting Multiple Sequence Alignment"
+print "Starting Multiple Sequence Alignment."
 
 # Start the Multiple Sequence Alignments
-os.system('mafft --clustalout workdir/input.fas > workdir/mafft_output.phy')
-os.system('muscle -in workdir/input.fas -clw -out workdir/muscle_output.phy')
+os.system('mafft --clustalout workdir/input.fas > workdir/mafft_output.fas')
+#os.system('muscle -in workdir/input.fas -clw -out workdir/muscle_output.fas')
 
+# Open MSA output files, readlines, and store in lists
+#Format_muscle = open("workdir/muscle_output.fas")
+Format_mafft = open("workdir/mafft_output.fas")
+#muscle = Format_muscle.readlines()
+mafft = Format_mafft.readlines()
+
+# While loop to select all lines per sequence from the outputfiles
+i = 0
+while i < seq_num:
+#	muscle[i+3::22]
+	print mafft[i+3::22]
+	i += 1
