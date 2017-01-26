40d72578f79b5fa09eddee8b88f2e5a2b8bea7c7
Helen <helen@NotebookHelen>
2017-01-26 13:48:06 +0100

    Updated pipeline to get the tree

--- pipeline.py
+++ pipeline.py
@@ -80,37 +80,42 @@ while i < seq_blocks:
 	i += 1
 
 # Double while loop to write PHYLIP format to output file
-out=open("workdir/mafft_output.phy", "w")
-out.write(str(seq_num)+"\t"+str(seq_len)+"\n") # Write PHYLIP header
-i = 0
-while i < seq_blocks:
-	j = 0
-	while j < seq_num:
-		out.write(dictio[j][i]) # Write blocks of sequences to file
-		j += 1
-	out.write('\n') # Give a newline in between blocks
-	i += 1
-out.close()
+#out=open("workdir/mafft_output.phy", "w")
+#out.write(str(seq_num)+"\t"+str(seq_len)+"\n") # Write PHYLIP header
+#i = 0
+#while i < seq_blocks:
+#	j = 0
+#	while j < seq_num:
+#		out.write(dictio[j][i]) # Write blocks of sequences to file
+#		j += 1
+#	out.write('\n') # Give a newline in between blocks
+#	i += 1
+#out.close()
 
 # rm phylip files first! - clean up last run
 #os.system("rm workdir/infile workdir/outfile workdir/infile2 workdir/outtree workdir/")
 
 #To create the input files used with the phylip commands. This will use the default settings of each function.
-os.system("echo 'mafft_output.phy' > workdir/input")
+os.system("echo 'workdir/mafft_output.phy' > workdir/input")
 os.system("echo 'Y' >> workdir/input")
 os.system("echo '' >> workdir/input")
 
-os.system("echo 'distance.dat' > workdir/input2")
+os.system("echo 'workdir/distance.dat' > workdir/input2")
 os.system("echo 'Y' >> workdir/input2")
 os.system("echo '' >> workdir/input2")
 
-# if string[-3:] == "fnt"
-# 	phylip dnadist < input > screenout
-# elif string[-3:] == "faa"
-# 	phylip protdist < input > screenout
-# mv outfile > workdir/distance.dat
-# phylip neighbor < infile2 > screenout
+if string[-3:] == "fnt":
+	os.system("phylip dnadist < workdir/input")
+elif string[-3:] == "faa":
+	os.system("phylip protdist < workdir/input")
+os.system("mv outfile workdir/distance.dat")
+os.system("phylip neighbor < workdir/input2")
+os.system("mv outfile workdir/output_tree")
+os.system("mv outtree workdir/phylo_tree")
 
 # Instead of phylip draw function, biopython could be used for tree visualization
 
+#from Bio import Phylo
+#tree = Phylo.read("workdir/outtree", "newick")
+#Phylo.draw_ascii(tree)
 
